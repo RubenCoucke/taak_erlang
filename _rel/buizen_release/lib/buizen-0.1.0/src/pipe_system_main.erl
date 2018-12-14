1ecce31abe211940f8777ad49c48fa4e1ebc1893
Lemare <couckeruben1@gmail.com>
2018-12-14 14:59:09 +0100

    great improvements

--- _rel/buizen_release/lib/buizen-0.1.0/src/pipe_system_main.erl
+++ _rel/buizen_release/lib/buizen-0.1.0/src/pipe_system_main.erl
@@ -2,5 +2,9 @@
 -export([init/0]).
 
 init()->
+    register(main_system, self()),
+    survivor:start(),
+    observer:start(),
     {ok, Pipe1} = resource_type:create(pipeTyp, []),
     {ok, PipeInst1} = pipeInst:create(self(), Pipe1).
+    
